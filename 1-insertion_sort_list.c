f159d3d304bed7b1a53ac6b7c7c381ca75f80573
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-26 12:53:47 +0000

    sdsd

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,34 +1,35 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- * in ascending order using the Insertion sort algorithm
- * @list: Pointer to the head of the list
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm
+ * @list: Pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *tmp;
+    listint_t *curr = NULL, *prev = NULL;
 
-	if (!list || !*list || !(*list)->next)
-		return;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	current = (*list)->next;
-	while (current)
-	{
-		tmp = current;
-		while (tmp->prev && tmp->n < tmp->prev->n)
-		{
-			tmp->prev->next = tmp->next;
-			if (tmp->next)
-				tmp->next->prev = tmp->prev;
-			tmp->next = tmp->prev;
-			tmp->prev = tmp->prev->prev;
-			tmp->next->prev = tmp;
-			if (tmp->prev)
-				tmp->prev->next = tmp;
-			else
-				*list = tmp;
-		}
-		current = current->next;
-	}
+    curr = (*list)->next;
+    while (curr != NULL)
+    {
+        prev = curr->prev;
+        while (prev != NULL && prev->n > curr->n)
+        {
+            prev->next = curr->next;
+            if (curr->next != NULL)
+                curr->next->prev = prev;
+            curr->next = prev;
+            curr->prev = prev->prev;
+            prev->prev = curr;
+            if (curr->prev != NULL)
+                curr->prev->next = curr;
+            else
+                *list = curr;
+            prev = curr->prev;
+            print_list(*list);
+        }
+        curr = curr->next;
+    }
 }
